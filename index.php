cc44ed9d72896d07001336fbe0089bbac4c9e2a1
Williams <Williams@Williams-PC>
2016-01-27 09:39:09 -0400

    css

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -19,7 +14,7 @@ and open the template in the editor.
         
         <title></title>
     </head>
-    <body> 
+    <body id="bodyprincipal"> 
         <div id="divprincipal">
        <center>
         <menu id="idmenu"  >
